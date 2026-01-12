588c3918290f3075559b34634959472cc220bc98
sofiacr0 <pelistvshows@gmail.com>
2026-01-12 03:43:26 +0100

    firebase startup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
         // Import the Firebase BoM
         implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
 
-
-        // TODO: Add the dependencies for Firebase products you want to use
         // When using the BoM, don't specify versions in Firebase dependencies
         implementation("com.google.firebase:firebase-analytics")
         implementation("com.google.firebase:firebase-auth")
